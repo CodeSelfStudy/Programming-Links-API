538570741e534206d68d81687676fe5a751ef24d
J. Cohen <hostelmarketing@gmail.com>
2015-12-16 12:24:10 -0800

    Update schema and README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Programming-Links-API
+# Programming Links API
+
 A group project to display programming links on the site.
+
+## TODO
+
+* Implement Github authentication
+* Allow users to comment on links
+* Allow users to add links
+
+## Examples
+
+Example of posting a link:
+
+    curl -d '[{"title": "Learn Python the Hard Way", "url": "http://learnpythonthehardway.org/book/", "description": "A book that teaches you how to program computuers with Python"}]' -H 'Content-Type: application/json'  http://127.0.0.1:5000/links
--- app/settings.py
+++ app/settings.py
@@ -1,15 +1,4 @@
-DOMAIN = {'links': {}}
-
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-# TODO: get from env
-MONGO_DBNAME = 'linksapi'
-
-# Global resource and item methods
-RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
-ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
-
-schema = {
+link_schema = {
     'title': {
         'type': 'string',
         'minlength': 1,
@@ -29,5 +18,26 @@ schema = {
     'tags': {
         'type': 'list',
         'items': [{'type': 'string'}],
+    },
+    'comments': {
+        'type': 'list',
+        'items': [{'type': 'string'}],
     }
 }
+
+links = {
+    'schema': link_schema,
+}
+
+DOMAIN = {
+    'links': links
+}
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+# TODO: get from env
+MONGO_DBNAME = 'linksapi'
+
+# Global resource and item methods
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
