2711ec69332938f8a54ec1267829cc4608ceb525
J. Cohen <hostelmarketing@gmail.com>
2015-12-16 12:02:40 -0800

    Define schema

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,33 @@
 DOMAIN = {'links': {}}
 
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+# TODO: get from env
+MONGO_DBNAME = 'linksapi'
 
+# Global resource and item methods
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+schema = {
+    'title': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required':True,
+    },
+    'url': {
+        'type': 'string',
+        'minlength': 11, # e.g., http://t.co
+        'maxlength': 2083, # IE limit,
+        'required':True,
+    },
+    'description': {
+        'type': 'string',
+
+    },
+    'tags': {
+        'type': 'list',
+        'items': [{'type': 'string'}],
+    }
+}
